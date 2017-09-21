a0d057279b82dd91c11bbd5cf9ffdd193e53080a
Joebob94 <nikpla106@gmail.com>
2017-09-21 15:51:29 +0200

    Tasks kann man löschen und editieren

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,14 @@
 class ItemsController < ApplicationController
+
 	def index
 		@items = Item.all.order("created_at DESC")
 	end
 
+
 	def new
 		@item = Item.new
 	end
+
 
 	def create
 		@item = Item.new(item_params)
@@ -18,13 +21,35 @@ class ItemsController < ApplicationController
 
 	end
 
+
 	def show
 		@item = Item.find(params[:id])
 	end
 
-	def destroy
+
+	def edit
 		@item = Item.find(params[:id])
 	end
+
+
+	def update 
+		@item = Item.find(params[:id])
+
+		if @item.update(item_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
+
+	end
+
+	def destroy
+		@item = Item.find(params[:id])
+
+		@item.destroy
+		redirect_to root_path
+	end
+
 
 	private def item_params
 		params.require(:item).permit(:title, :description)
