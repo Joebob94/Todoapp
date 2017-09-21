1bd16797f3878ec55ec85250ea75b292da33f9d4
Joebob94 <nikpla106@gmail.com>
2017-09-21 17:59:19 +0200

    'Erledigt' Funktion hinzugefügt

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920152808) do
+ActiveRecord::Schema.define(version: 20170921150908) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
   end
 
 end
